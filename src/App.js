4ad439de45aa0eae94931748d82fc768a8df88ea
Ryan Vandersmith <ryanvandersmith@gmail.com>
2022-07-27 13:13:12 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+import CodeEditor from "./CodeEditor";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CodeEditor />
+    </div>
+  );
+}
---
+++ src/CodeEditor.js
@@ -0,0 +1,50 @@
+import MonacoEditor from "@monaco-editor/react";
+import { configureMonaco } from "./configureMonaco";
+// import useThemeState from "../../hooks/persistent/useThemeState";
+
+export default function CodeEditor({
+  value,
+  onChange,
+  readOnly,
+  options,
+  ...others
+}) {
+  // const monaco = useMonaco();
+  // const [theme] = useThemeState();
+  const theme = "vs-dark"; // 'night-owl'
+
+  const onEditorChange = (newValue) => {
+    onChange?.(newValue);
+  };
+
+  return (
+    <MonacoEditor
+      // width="30vh"
+      // height="40vh"
+      theme={theme}
+      defaultLanguage="motoko"
+      beforeMount={configureMonaco}
+      // onMount={(editor, monaco) => {
+      //     setTimeout(() => {
+      //         console.log(editor.getAction('editor.action.formatDocument').run());
+      //
+      //         // console.log(editor.trigger('actor{public func test() : Text {}}', 'editor.action.formatDocument'));
+      //     },500);
+      // }}
+      value={value}
+      // path={fileName}
+      onChange={onEditorChange}
+      options={{
+        tabSize: 2,
+        minimap: { enabled: false },
+        wordWrap: "off",
+        // wrappingIndent: 'indent',
+        scrollBeyondLastLine: false,
+        fontSize: 16,
+        readOnly,
+        ...options
+      }}
+      {...others}
+    />
+  );
+}
