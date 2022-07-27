4748fb58dea6f89fc03bcbdc77c52f8df5887281
Ryan Vandersmith <ryanvandersmith@gmail.com>
2022-07-27 17:42:50 -0600

    Sync

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import "./styles.css";
 
 import { useState, useMemo } from "react";
-import CodeEditor from "./CodeEditor";
+import { FaCode } from "react-icons/fa";
 import { Motoko } from "motoko";
+import copy from "copy-to-clipboard";
+import CodeEditor from "./CodeEditor";
 
-const defaultCode =
-  `
-// A simple Motoko smart contract.
+const EMBED_LINK_BASE =
+  "https://codesandbox.io/embed/motoko-44l2v4?view=preview&hidenavigation=1&hidedevtools=1&initialpath=";
 
-actor Main {
-  public func hello() : async Text {
-    "Hello, world!"
+const UNCOMPRESSED_FORMAT = "c";
+
+const shareData = window.location.pathname.substring(1);
+let defaultCode;
+if (shareData) {
+  if (shareData.startsWith(UNCOMPRESSED_FORMAT)) {
+    try {
+      defaultCode = atob(shareData.substring(UNCOMPRESSED_FORMAT.length));
+    } catch (err) {
+      defaultCode = "// Unable to parse share link";
+    }
+  }
+} else {
+  defaultCode =
+    `
+  // A simple Motoko smart contract.
+
+  actor Main {
+    public func hello() : async Text {
+      "Hello, world!"
+    };
   };
-};
 
-await Main.hello()
-`.trim() + "\n";
+  await Main.hello()
+  `.trim() + "\n";
+}
 
 export default function App() {
   const [value, setValue] = useState(defaultCode);
+  const [message, setMessage] = useState(null);
 
   const output = useMemo(() => {
     try {
@@ -29,6 +49,18 @@ export default function App() {
     }
   }, [value]);
 
+  const copyEmbedLink = () => {
+    const format = UNCOMPRESSED_FORMAT;
+    const link = `${EMBED_LINK_BASE}${format}${btoa(value)}`;
+    if (link.length >= 2048) {
+      alert("Your code is too long to fit into a URL!");
+      return;
+    }
+    copy(link);
+    setMessage(`Copied share link to clipboard:\n${link}`);
+    setTimeout(() => setMessage(null), 2000);
+  };
+
   const consoleHeight = 100;
 
   return (
@@ -38,29 +70,35 @@ export default function App() {
         onChange={setValue}
         height={`calc(100vh - ${consoleHeight}px)`}
       />
-      {!!output && (
-        <div
-          style={{
-            fontSize: 14,
-            padding: "5px 20px",
-            textAlign: "left",
-            maxWidth: "100vw",
-            maxHeight: consoleHeight,
-            overflowY: "auto"
-          }}
-        >
-          {!!output.stdout && (
-            <pre style={{ color: "lightgreen", overflow: "auto" }}>
-              {output.stdout}
-            </pre>
-          )}
-          {!!output.stderr && (
-            <pre style={{ color: "pink", opacity: 0.8, overflow: "auto" }}>
-              {output.stderr}
-            </pre>
-          )}
+      <div className="button-menu">
+        <div className="button" onClick={copyEmbedLink}>
+          <FaCode />
         </div>
-      )}
+      </div>
+      <div
+        style={{
+          fontSize: 16,
+          padding: "5px 20px",
+          textAlign: "left",
+          maxWidth: "100vw",
+          maxHeight: consoleHeight,
+          overflowY: "auto"
+        }}
+      >
+        {!!message && (
+          <pre style={{ color: "white", overflow: "auto" }}>{message}</pre>
+        )}
+        {!!output?.stdout && (
+          <pre style={{ color: "lightgreen", overflow: "auto" }}>
+            {output.stdout}
+          </pre>
+        )}
+        {!!output?.stderr && (
+          <pre style={{ color: "pink", opacity: 0.8, overflow: "auto" }}>
+            {output.stderr}
+          </pre>
+        )}
+      </div>
     </div>
   );
 }
